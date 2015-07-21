103c66171aca93043eba65f5010b456a59fea335
Lee Ellam <lee.ellam@justgiving.com>
2015-07-21 07:58:39 +0100

    add devdependencies

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
-module.exports = gulp.task('lint', function() {
-  return gulp.src('./src/**/*.js')
-     .pipe(jshint('.jshintrc'))
-     .pipe(jshint.reporter(stylish));
-});
+module.exports = function(gulp) {
+  gulp.task('lint', function() {
+    return gulp.src('./src/**/*.js')
+       .pipe(jshint('.jshintrc'))
+       .pipe(jshint.reporter(stylish));
+  });
+};
