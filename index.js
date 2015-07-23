3e3cea344f8e5ef586e7b007910fd6bd88ac1be1
Lee Ellam <lee.ellam@justgiving.com>
2015-07-23 11:55:37 +0100

    remove console log

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var stylish = require('jshint-stylish');
 
 module.exports = function(gulp, config) {
   return gulp.task('lint', function() {
-    console.log('linting')
     return gulp.src(config.src)
        .pipe(jshint(config.jshintrc))
        .pipe(jshint.reporter(stylish));
