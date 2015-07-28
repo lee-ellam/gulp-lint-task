69063c7147b2f65da69994321ac2437f315c4613
Lee Ellam <lee.ellam@justgiving.com>
2015-07-28 09:03:43 +0100

    changes to git tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var lint = require('./lib');
 var mocha = require('gulp-mocha');
 var git = require('gulp-git');
 var bump = require('gulp-bump');
+var sequence = require('run-sequence');
 
 // Lint task - Meta AF
 gulp.task('lint', lint({ src: './lib/*.js' }));
@@ -41,31 +42,45 @@ gulp.task('commit', function() {
   var message = 'Release ' + v;
 
   return gulp.src('.')
-    .pipe(git.commit(message));
+    .pipe(git.commit('Bumped version number'));
+});
+
+// Push changes
+gulp.task('push', function(cb) {
+  git.push('origin', 'master', cb);
 });
 
 // Commit and tag release in git
-gulp.task('tag', function() {
+gulp.task('tag', function(cb) {
   var pkg = require('./package.json');
   var v = 'v' + pkg.version;
   var message = 'Tag for release ' + v;
 
   git.tag(v, message, function(err) {
     if (err) {
-      throw err;
-    } else {    
-      git.push('origin', 'master', { args: '--tags' }, function(err) {
-        if (err) {
-          throw err;
-        } else {
-          console.log('Release successful');
-        }
-      });
+      return cb(err);
+    } else {
+      git.push('origin', 'master', { args: '--tags' }, cb);
     }
   });
 });
 
 // Release to git
-gulp.task('release', ['bump', 'tag']);
-gulp.task('release:minor', ['bump:minor', 'tag']);
-gulp.task('release:major', ['bump:major', 'tag']);
+gulp.task('release', function(cb) {
+  sequence('bump', 'commit', 'push', 'tag', fn);
+});
+gulp.task('release:minor', function(cb) {
+  sequence('bump:minor', 'commit', 'push', 'tag', fn);
+});
+gulp.task('release:major', function(cb) {
+  sequence('bump:major', 'commit', 'push', 'tag', fn);
+});
+
+function fn(err) {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log('Released successfully');
+  }
+  cb(err);
+}
